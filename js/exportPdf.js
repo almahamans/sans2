dcfd22cc7492b4f3d4ba3480ad83204e51734872
Almaha <mahaw999@gmail.com>
2023-11-15 15:59:40 +0300

    adding export pdf properity

--- js/exportPdf.js
+++ js/exportPdf.js
@@ -1,25 +1,98 @@
+(function () {  
+  var form = $('body'),  
+   cache_width = form.width(),  
+   a4 = [595.28, 841.89]; // for a4 size paper width and height  
 
-// let pdf = new pdf('p', 'pt', 'letter');
-// pdf.canvas.height = 72 * 11;
-// pdf.canvas.width = 72 * 8.5;
+  $('#pdf').on('click', function () {  
+      $('body').scrollTop(0);  
+      document.getElementById('icons').style.display = 'none' 
+      createPDF(); 
+  });  
+  //create pdf  
+  function createPDF() {  
+      getCanvas().then(function (canvas) {  
+          var  
+           img = canvas.toDataURL("image/png"),  
+           doc = new jsPDF({  
+               unit: 'px',  
+               format: 'a4'  
+           });  
+          doc.addImage(img, 'JPEG', 20, 20);  
+          doc.save(`${document.getElementsByTagName('h1')[0].innerText}.pdf`);  
+          form.width(cache_width);  
+      });  
+  }  
 
-// document.getElementById('pdf').addEventListener('click', function() {
-//   html2canvas(document.getElementById('contentToPrint'), {
-//     canvas: pdf.canvas,
-//     onrendered: function(canvas) {
-//       pdf.save('download.pdf');
-//     }
-//   });
-// }); 
+  // create canvas object  
+  function getCanvas() {  
+      form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');  
+      return html2canvas(form, {  
+          imageTimeout: 2000,  
+          removeContainer: true  
+      });  
+  }  
 
-document.getElementById("pdf").addEventListener("click", function () {
-    var element = document.getElementsByTagName('body');
-    var opt = {
-      margin:       1,
-      filename:     'myfile.pdf',
-      image:        { type: 'jpeg', quality: 0.98 },
-      html2canvas:  { scale: 2 },
-      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-    };
-    html2pdf().from(element).set(opt).save('changeRequirment.pdf');
-});
+}());  
+ 
+/* 
+* jQuery helper plugin for examples and tests 
+*/  
+(function ($) {  
+  $.fn.html2canvas = function (options) {  
+      var date = new Date(),  
+      $message = null,  
+      timeoutTimer = false,  
+      timer = date.getTime();  
+      html2canvas.logging = options && options.logging;  
+      html2canvas.Preload(this[0], $.extend({  
+          complete: function (images) {  
+              var queue = html2canvas.Parse(this[0], images, options),  
+              $canvas = $(html2canvas.Renderer(queue, options)),  
+              finishTime = new Date();  
+
+              $canvas.css({ position: 'absolute', left: 0, top: 0 }).appendTo(document.body);  
+              $canvas.siblings().toggle();  
+
+              $(window).click(function () {  
+                  if (!$canvas.is(':visible')) {  
+                      $canvas.toggle().siblings().toggle();  
+                      throwMessage("Canvas Render visible");  
+                  } else {  
+                      $canvas.siblings().toggle();  
+                      $canvas.toggle();  
+                      throwMessage("Canvas Render hidden");  
+                  }  
+              });  
+              throwMessage('Screenshot created in ' + ((finishTime.getTime() - timer) / 1000) + " seconds<br />", 4000);  
+          }  
+      }, options));  
+
+      function throwMessage(msg, duration) {  
+          window.clearTimeout(timeoutTimer);  
+          timeoutTimer = window.setTimeout(function () {  
+              $message.fadeOut(function () {  
+                  $message.remove();  
+              });  
+          }, duration || 2000);  
+          if ($message)  
+              $message.remove();  
+          $message = $('<div ></div>').html(msg).css({  
+              margin: 0,  
+              padding: 10,  
+              background: "#000",  
+              opacity: 0.7,  
+              position: "fixed",  
+              top: 10,  
+              right: 10,  
+              fontFamily: 'Tahoma',  
+              color: '#fff',  
+              fontSize: 12,  
+              borderRadius: 12,  
+              width: 'auto',  
+              height: 'auto',  
+              textAlign: 'center',  
+              textDecoration: 'none'  
+          }).hide().fadeIn().appendTo('body');  
+      }  
+  };  
+})(jQuery);  
