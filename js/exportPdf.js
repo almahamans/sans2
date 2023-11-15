a6432d26cf3c8a562f4d0da347aaff26a8098fc6
Almaha <mahaw999@gmail.com>
2023-11-15 13:53:45 +0300

    fix bugs

--- js/exportPdf.js
+++ js/exportPdf.js
@@ -1,13 +1,25 @@
 
-let pdf = new pdf('p', 'pt', 'letter');
-pdf.canvas.height = 72 * 11;
-pdf.canvas.width = 72 * 8.5;
+// let pdf = new pdf('p', 'pt', 'letter');
+// pdf.canvas.height = 72 * 11;
+// pdf.canvas.width = 72 * 8.5;
 
-document.getElementById('pdf').addEventListener('click', function() {
-  html2canvas(document.getElementById('contentToPrint'), {
-    canvas: pdf.canvas,
-    onrendered: function(canvas) {
-      pdf.save('download.pdf');
-    }
-  });
+// document.getElementById('pdf').addEventListener('click', function() {
+//   html2canvas(document.getElementById('contentToPrint'), {
+//     canvas: pdf.canvas,
+//     onrendered: function(canvas) {
+//       pdf.save('download.pdf');
+//     }
+//   });
+// }); 
+
+document.getElementById("pdf").addEventListener("click", function () {
+    var element = document.getElementsByTagName('body');
+    var opt = {
+      margin:       1,
+      filename:     'myfile.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+    html2pdf().from(element).set(opt).save('changeRequirment.pdf');
 });
--- js/uploadfile.js
+++ js/uploadfile.js
@@ -1,13 +1,3 @@
-document.getElementById('fileUpload').addEventListener('change', function(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-    
-    reader.onloadend = function() {
-      console.log('Result', reader.result);
-    }
-    
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-    sessionStorage.setItem('file', reader.result)
-  });
+document.getElementById('uploadButton').addEventListener('click', function() {
+  document.getElementById('fileUpload').click();
+});
