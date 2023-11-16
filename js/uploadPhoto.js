ae43685db40215e9788f226e7291235e6c1420e5
Almaha <mahaw999@gmail.com>
2023-11-16 03:26:44 +0300

    update the code

--- js/uploadPhoto.js
+++ js/uploadPhoto.js
@@ -1,6 +1,6 @@
 document.getElementById('uploadButtonPh').addEventListener('click', function() {
     document.getElementById('pPhUpload').click();
   });
-  document.getElementById('uploadButtonS').addEventListener('click', function() {
+document.getElementById('uploadButtonS').addEventListener('click', function() {
     document.getElementById('signUpload').click();
   });
